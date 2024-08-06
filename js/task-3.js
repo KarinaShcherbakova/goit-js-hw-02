3d788b440f13c510dba24fb28167c650f6136294
Karina_Shcherbakova <karina.shcherbakova18@gmail.com>
2024-08-06 19:25:32 +0200

    import new js files

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+function checkForSpam(message) {
+    let messageLower = message.toLowerCase();
+    let messageLowerIncludesSpam = messageLower.includes("spam");
+    let messageLowerIncludesSale = messageLower.includes("sale"); 
+    let result;
+    if (messageLowerIncludesSpam || messageLowerIncludesSale) {
+          result = true;
+    }
+    else {
+          result = false;
+    }
+    return result;
+  }
+  
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
